a2fec52eb3f6412ac9c0121e1c92052157caf97d
TaniqueRaynor <tanique.raynor@gmail.com>
2025-08-05 23:00:25 -0500

    update vpc

--- main.tf
+++ main.tf
@@ -14,13 +14,12 @@ data "aws_ami" "app_ami" {
   owners = ["979382823631"] # Bitnami
 }
 
-data "aws_vpc" "default"{
-   default=true
-}
 
 resource "aws_instance" "web" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
+
+  subnet_id = module.web_vpc.public_subnets[0]
 
   vpc_security_group_ids = [module.web_sg.security_group_id]
 
@@ -29,12 +28,28 @@ resource "aws_instance" "web" {
   }
 }
 
+module "web_vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+
+  name = "dev"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  tags = {
+    Terraform = "true"
+    Environment = "dev"
+  }
+}
+
 module "web_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "5.3.0"
   name    = "web_new"
 
-  vpc_id  = data.aws_vpc.default.id
+  vpc_id  = module.web_vpc.vpc_id
 
   ingress_rules       = ["http-80-tcp","https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
